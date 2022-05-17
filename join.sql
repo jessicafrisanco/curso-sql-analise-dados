e5b1a89709a174af42f2e15e8b42ba8c8e3f4397
Jéssica Frisanco <102257826+jessicafrisanco@users.noreply.github.com>
2022-05-17 15:07:06 -0300

    Create join.sql

---
+++ join.sql
@@ -0,0 +1,13 @@
+#JOIN
+
+select
+  i.id as item_id,
+  i.order_id,
+  i.quantity,
+  i.total_price,
+  i.product_id,
+  p.id,
+  p.name
+from e_commerce.items as i
+join e_commerce.products as p on i.product_id = p.id;
+  
